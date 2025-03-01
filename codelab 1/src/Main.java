e146a5af7bcecf839f3333ef8c03b52202bd61de
Yoga_150 <muhammadyogaadraassajad@gmail.com>
2025-03-02 02:28:56 +0700

    Main.java

--- codelab 1/src/Main.java
+++ codelab 1/src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner; // digunakan untuk mendeklarasikan scanner untuk input user atau pengguna.
+import java.time.LocalDate; // digunakan untuk mendeklarasikan localdate atau informasi waktu saat ini.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main { // digunakan untuk deklarasi class utama.
+    public static void main(String[] args) { // digunakan sebagai titik awal eksekusi program java.
+        Scanner scanner = new Scanner(System.in); // digunakan untuk input user atau pengguna.
+        System.out.print("Masukkan nama: "); // digunakan untuk menampilkan tulisan.
+        String nama = scanner.nextLine(); // digunakan untuk membaca input user.
+        System.out.print("Masukkan jenis kelamin (P/L): "); // digunakan untuk menampilkan tulisan.
+
+        char jenisKelamin = scanner.next().charAt(0); // digunakan untuk membaca input.
+        System.out.print("Masukkan tahun lahir: "); // menampilkan tulisan tahun lahir.
+
+        int tahunLahir = scanner.nextInt(); // digunakan untuk input tahun lahir.
+        int tahunSekarang = LocalDate.now().getYear(); // digunakan untuk mengambil informasi tahun ini.
+        int umur = tahunSekarang - tahunLahir; // menghitung umur pengguna dengan mengurangi tahun lahir dengan tahun sekarang.
+        String jenisKelaminStr = (jenisKelamin == 'L' || jenisKelamin == 'l') ? "Laki-laki" : "Perempuan"; // digunakan untuk mengkonversi jenis kelamin laki-laki atau perempuan dari huruf.
+        System.out.println("\nData Diri:");
+        System.out.println("Nama          : " + nama);
+        System.out.println("Jenis Kelamin : " + jenisKelaminStr);        // keempat syntax ini menampilkan data diri.
+        System.out.println("Umur          : " + umur + " tahun");
+
+
+        scanner.close(); // digunakan untuk menutup scanner.
     }
 }
